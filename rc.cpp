785aa94cec4419bb768c9f2251dbde6ca5452c3e
Charles Barcza <info@blackpanther.hu>
2017-05-06 21:03:09 +0200

    Add translation support

---
+++ rc.cpp
@@ -0,0 +1,132 @@
+// i18n: file: espeak_qtgui/ui/gui.ui:14
+// i18n: ectx: property (windowTitle), widget (QMainWindow, MainWindow)
+i18n("espeak-qtgui");
+// i18n: file: espeak_qtgui/ui/gui.ui:23
+// i18n: ectx: property (text), widget (QPushButton, new_button)
+i18n("New");
+// i18n: file: espeak_qtgui/ui/gui.ui:30
+// i18n: ectx: property (text), widget (QPushButton, open_button)
+i18n("Open");
+// i18n: file: espeak_qtgui/ui/gui.ui:37
+// i18n: ectx: property (text), widget (QPushButton, save_as_button)
+i18n("Save As");
+// i18n: file: espeak_qtgui/ui/gui.ui:51
+// i18n: ectx: property (text), widget (QPushButton, record_button)
+i18n("Record");
+// i18n: file: espeak_qtgui/ui/gui.ui:65
+// i18n: ectx: property (text), widget (QPushButton, revert_button)
+i18n("Revert");
+// i18n: file: espeak_qtgui/ui/gui.ui:74
+// i18n: ectx: property (text), widget (QLabel, label)
+i18n("Insert text to play");
+// i18n: file: espeak_qtgui/ui/gui.ui:86
+// i18n: ectx: property (text), widget (QLabel, label_2)
+i18n("Base settings");
+// i18n: file: espeak_qtgui/ui/gui.ui:106
+// i18n: ectx: property (text), widget (QPushButton, play_button)
+i18n("Play");
+// i18n: file: espeak_qtgui/ui/gui.ui:113
+// i18n: ectx: property (text), widget (QPushButton, stop_button)
+i18n("Stop");
+// i18n: file: espeak_qtgui/ui/gui.ui:124
+// i18n: ectx: property (text), widget (QLabel, label_3)
+i18n("Language");
+// i18n: file: espeak_qtgui/ui/gui.ui:132
+// i18n: ectx: property (text), item, widget (QComboBox, language_comboBox)
+i18n("Hungarian");
+// i18n: file: espeak_qtgui/ui/gui.ui:137
+// i18n: ectx: property (text), item, widget (QComboBox, language_comboBox)
+i18n("English");
+// i18n: file: espeak_qtgui/ui/gui.ui:162
+// i18n: ectx: property (text), widget (QLabel, label_4)
+i18n("Voice");
+// i18n: file: espeak_qtgui/ui/gui.ui:171
+// i18n: ectx: property (text), widget (QRadioButton, male_radioButton)
+i18n("&Male");
+// i18n: file: espeak_qtgui/ui/gui.ui:181
+// i18n: ectx: property (text), widget (QRadioButton, female_radioButton)
+i18n("Fe&male");
+// i18n: file: espeak_qtgui/ui/gui.ui:197
+// i18n: ectx: property (text), widget (QLabel, label_10)
+i18n("Variant:");
+// i18n: file: espeak_qtgui/ui/gui.ui:229
+// i18n: ectx: property (text), widget (QLabel, label_5)
+i18n("Advanced settings");
+// i18n: file: espeak_qtgui/ui/gui.ui:244
+// i18n: ectx: property (text), widget (QLabel, label_6)
+i18n("Pitch:");
+// i18n: file: espeak_qtgui/ui/gui.ui:287
+// i18n: ectx: property (text), widget (QLabel, label_7)
+i18n("Volume:");
+// i18n: file: espeak_qtgui/ui/gui.ui:330
+// i18n: ectx: property (text), widget (QLabel, label_8)
+i18n("Speed:");
+// i18n: file: espeak_qtgui/ui/gui.ui:373
+// i18n: ectx: property (text), widget (QLabel, label_9)
+i18n("Delay:");
+// i18n: file: espeak_qtgui/ui/gui.ui:422
+// i18n: ectx: property (title), widget (QMenu, menuFile)
+i18n("F&ile");
+// i18n: file: espeak_qtgui/ui/gui.ui:432
+// i18n: ectx: property (title), widget (QMenu, menuEdit)
+i18n("E&dit");
+// i18n: file: espeak_qtgui/ui/gui.ui:442
+// i18n: ectx: property (title), widget (QMenu, menuHelp)
+i18n("He&lp");
+// i18n: file: espeak_qtgui/ui/gui.ui:452
+// i18n: ectx: property (text), widget (QMenu, menuHelp)
+i18n("&Exit");
+// i18n: file: espeak_qtgui/ui/gui.ui:455
+// i18n: ectx: property (shortcut), widget (QMenu, menuHelp)
+i18n("Ctrl+Q");
+// i18n: file: espeak_qtgui/ui/gui.ui:460
+// i18n: ectx: property (text), widget (QMenu, menuHelp)
+i18n("New");
+// i18n: file: espeak_qtgui/ui/gui.ui:463
+// i18n: ectx: property (shortcut), widget (QMenu, menuHelp)
+i18n("Ctrl+N");
+// i18n: file: espeak_qtgui/ui/gui.ui:468
+// i18n: ectx: property (text), widget (QMenu, menuHelp)
+i18n("Open");
+// i18n: file: espeak_qtgui/ui/gui.ui:471
+// i18n: ectx: property (shortcut), widget (QMenu, menuHelp)
+i18n("Ctrl+O");
+// i18n: file: espeak_qtgui/ui/gui.ui:476
+// i18n: ectx: property (text), widget (QMenu, menuHelp)
+i18n("Save As");
+// i18n: file: espeak_qtgui/ui/gui.ui:479
+// i18n: ectx: property (shortcut), widget (QMenu, menuHelp)
+i18n("Ctrl+S");
+// i18n: file: espeak_qtgui/ui/gui.ui:484
+// i18n: ectx: property (text), widget (QMenu, menuHelp)
+i18n("Quit");
+// i18n: file: espeak_qtgui/ui/gui.ui:487
+// i18n: ectx: property (shortcut), widget (QMenu, menuHelp)
+i18n("Ctrl+Q");
+// i18n: file: espeak_qtgui/ui/gui.ui:492
+// i18n: ectx: property (text), widget (QMenu, menuHelp)
+i18n("Play");
+// i18n: file: espeak_qtgui/ui/gui.ui:495
+// i18n: ectx: property (shortcut), widget (QMenu, menuHelp)
+i18n("Ctrl+P");
+// i18n: file: espeak_qtgui/ui/gui.ui:500
+// i18n: ectx: property (text), widget (QMenu, menuHelp)
+i18n("Stop");
+// i18n: file: espeak_qtgui/ui/gui.ui:503
+// i18n: ectx: property (shortcut), widget (QMenu, menuHelp)
+i18n("Ctrl+K");
+// i18n: file: espeak_qtgui/ui/gui.ui:508
+// i18n: ectx: property (text), widget (QMenu, menuHelp)
+i18n("Record");
+// i18n: file: espeak_qtgui/ui/gui.ui:511
+// i18n: ectx: property (shortcut), widget (QMenu, menuHelp)
+i18n("Ctrl+R");
+// i18n: file: espeak_qtgui/ui/gui.ui:516
+// i18n: ectx: property (text), widget (QMenu, menuHelp)
+i18n("Revert");
+// i18n: file: espeak_qtgui/ui/gui.ui:519
+// i18n: ectx: property (shortcut), widget (QMenu, menuHelp)
+i18n("Ctrl+Del");
+// i18n: file: espeak_qtgui/ui/gui.ui:524
+// i18n: ectx: property (text), widget (QMenu, menuHelp)
+i18n("About");
